10fcda7a527efdf73653a724a96b4523b99855d2
Christopher Angbalu <x17366481@student.ncirl.ie>
2018-11-12 12:21:51 +0000

    Cofftastic

---
+++ external.js
@@ -0,0 +1,16 @@
+function vFunction(){
+	
+	var video = document.getElementById("myVideo");
+
+	var btn = document.getElementById("myBtn");
+	
+	if(video.paused){
+		video.play();
+		btn.innerHTML = "Pause";
+	}
+	else{
+		video.pause();
+		btn.innerHTML = "Play";
+		
+	}
+}
